fdc03c1ea2f17e843b73887f179be80bda19ae89
Ebtehal90 <ebtehal90@gmail.com>
2020-01-31 10:34:31 +0100

    Declare variables and concatenate

---
+++ script.js
@@ -0,0 +1,65 @@
+const FIRST_NAME = 'Ebtehal Abdalla';
+const LAST_NAME  = 'Mansur';
+
+let fullName = FIRST_NAME +" "+ LAST_NAME;
+
+const WORK_EXPERIENCE = `
+
+(Communication&Marketing)
+Web&Com 2018-2019
+Roman Catholic Archdiocese
+
+(Interpreter/Cultural Mediator) 
+EASO EUROPEAN ASYLUM SUPPORT OFFICE /Grecee and Italy 2016-2019
+ELIL European Lawyers in Lesvos/ Grecee 2017
+
+(Advocacy & Vulonteer)
+ECRE European council of Refugees and Exile 2018-2019
+Mosaico Refugees 2014-2018 
+
+(freelance Graphic & Web Designer)
+2016-present`;
+
+
+const STUDIES = `
+(Event Management and Marketing)
+European School of Economics/Rome  2014-2015
+
+(Certificate on Migration in Southern Europe)
+EPLO European Public Law Organization/Greece 2017
+
+(Informationa Techonolgy)
+Informatics University/ Libya 2007-2009
+
+(Bachelor of Business Administration)
+The university of Afriqa/ Libya
+
+(Linguistic High School Diploma)
+Dar Alyamama/Libya 2004-2007
+
+(Elementry Boarding School)
+Monte Rosa/Switzerland 2003 2004
+`;
+
+const SOFT_SKILLS = `
+
+(Digital skills)
+-Graphic Softwares Photoshop & after effect .
+-word PermissionRequest, spreedsheet, presentaion programs etc..
+-software for managing websites and blogs such as JavaScript HTML code.
+
+(Communicational skills)/(Managerial Organisational skills)
+`;
+
+const LANGUAGES = `English C2 / Italian A2 / Arabic Mother tongue`;
+
+const CV = `${FIRST_NAME} ${LAST_NAME}
+ ${WORK_EXPERIENCE}
+ ${STUDIES} 
+ ${SOFT_SKILLS} 
+ ${LANGUAGES}`;
+
+console.log(CV)
+
+// build your own cv 
+// work experience , your studies , your soft skills , and languages all of these should be variables that you can concatinate in a single cv variable 
